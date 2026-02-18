65f937e7c17f230e65e997b18831a5ed682dc7ad
Nishad112-cyber <nishadguptad@gmail.com>
2026-02-18 23:12:00 +0530

    today ok

--- DSA-String/Uppercase.java
+++ DSA-String/Uppercase.java
@@ -3,7 +3,6 @@
 public class Uppercase {
     public static  String toUpperCase(String str){
      StringBuilder sb =new StringBuilder("");
-
      char ch = Character.toUpperCase(str.charAt(0));
      sb.append(ch);
      for(int i=1; i<str.length(); i++){
